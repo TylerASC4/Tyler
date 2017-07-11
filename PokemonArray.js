238fdf69b8222b9186463ac7a352bf045f3d2d45
TylerASC4 <GOSHDARNETS@gmailcom>
2017-07-11 13:41:04 -0400

    first commit

---
+++ PokemonArray.js
@@ -0,0 +1,19 @@
+var PokemonRoster = ["pikachu","charzard","charmander","leafie","turtle"];
+var firstToFight = PokemonRoster[0];
+var secondToFight = PokemonRoster[2];
+PokemonRoster.pop();
+console.log(PokemonRoster);//['pikachu','charmander','leafie','turtle',]
+
+var names = ["paper","pen","pencil","rock" ,"scissors"]
+for(var i = 0; i<5; i++){
+    console.log(names[i])
+}
+
+for (var i = 0; i<5; PokemonRoster.length[js]; i++);{
+    console.log(PokemonRoster[i]);
+}
+
+for (var i = 5; i>0; PokemonRoster.length.; i++);{
+    console.log(pokemonRoster[i])
+}
+
